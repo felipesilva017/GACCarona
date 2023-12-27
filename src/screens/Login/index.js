a47bc8f9e158ee42f9f360a320b128bd2ce2b7bd
felipesilva017 <felipemsilva017@gmail.com>
2023-12-27 13:52:18 -0300

    modif login screen

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,18 +1,54 @@
-import { GluestackUIProvider, Text, Box, Center } from "@gluestack-ui/themed";
+import {
+  GluestackUIProvider,
+  Center,
+  Image,
+  Button,
+  ButtonText,
+  VStack,
+} from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
-import { Button } from "@gluestack-ui/config/build/theme";
+import { StyleSheet } from "react-native";
 
 const Login = () => {
   return (
     <GluestackUIProvider config={config}>
-      <Box width="100%" justifyContent="center" alignItems="center">
+      <VStack style={styles.container}>
         <Center>
-          <Text>GACCarona</Text>
-          <Button>Clique aqui</Button>
+          <Image
+            style={styles.image}
+            source={require("../../assets/logo-gac-login.png")}
+            alt={"Logo Aço cearense"}
+            resizeMode="contain"
+          ></Image>
         </Center>
-      </Box>
+        <Button
+          style={styles.button}
+          bgColor="#1D3F72"
+          $active-bgColor="#5699D2"
+        >
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </VStack>
     </GluestackUIProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  button: {
+    width: "80%",
+    borderRadius: 8,
+    position: "absolute",
+    bottom: 20,
+  },
+});
 
 export default Login;
