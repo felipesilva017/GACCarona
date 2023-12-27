3feb3aab845b62353c13c21c8c3209f49bbf2189
felipesilva017 <felipemsilva017@gmail.com>
2023-12-27 13:50:36 -0300

    edit app

--- App.js
+++ App.js
@@ -1,45 +1,13 @@
-import {
-  GluestackUIProvider,
-  Text,
-  Box,
-  Image,
-  Button,
-} from "@gluestack-ui/themed";
-import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
-import { StyleSheet } from "react-native";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
+import Login from "./src/screens/Login";
+import Form from "./src/screens/Form";
 
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
-      <Box style={styles.container}>
-        <Image
-          style={styles.image}
-          source={require("./src/assets/logo-gac-login.png")}
-          alt={"Logo Aço cearense"}
-        ></Image>
-        <Button style={styles.button}>
-          <Text>Teste</Text>
-        </Button>
-      </Box>
+      {/* <Login /> */}
+      <Form />
     </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: "100%",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 2,
-    borderColor: "#ff0000",
-  },
-  image: {
-    borderWidth: 1,
-    width: "59%",
-  },
-  button: {
-    marginTop: "60%",
-  },
-});
