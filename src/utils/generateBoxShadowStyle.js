040183fed065cbb110a9697835b894491c681402
felipesilva017 <felipemsilva017@gmail.com>
2024-01-05 14:06:58 -0300

    creating generateBoxShadow

---
+++ src/utils/generateBoxShadowStyle.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({});
+
+export const generateBoxShadowStyle = (
+  xOffset,
+  yOffset,
+  shadowColorIos,
+  shadowOpacity,
+  shadowRadius,
+  elevation,
+  shadowColorAndroid
+) => {
+  if (Platform.OS === "ios") {
+    styles.boxShadow = {
+      shadowColor: shadowColorIos,
+      shadowOffset: { width: xOffset, height: yOffset },
+      shadowOpacity,
+      shadowRadius,
+    };
+  } else if (Platform.OS === "android") {
+    styles.boxShadow = {
+      elevation,
+      shadowColor: shadowColorAndroid,
+    };
+  }
+};
