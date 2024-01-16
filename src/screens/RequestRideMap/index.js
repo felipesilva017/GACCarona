8589e27e0c529ffc7f6824c9051dc29f02267b46
felipesilva017 <felipemsilva017@gmail.com>
2024-01-16 09:11:21 -0300

    adding styles import

--- src/screens/RequestRideMap/index.js
+++ src/screens/RequestRideMap/index.js
@@ -12,6 +12,7 @@ import {
   getCurrentPositionAsync,
   LocationAccuracy,
 } from "expo-location";
+import { styles } from "./styles";
 
 const RequestRideMap = () => {
   const navigation = useNavigation();
@@ -108,44 +109,6 @@ const RequestRideMap = () => {
     </GluestackUIProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  content: {
-    flex: 1,
-  },
-  map: {
-    flex: 1,
-  },
-  selectLocation: {
-    borderWidth: 2,
-    width: "90%",
-    height: "12%",
-    position: "absolute",
-    top: "12%",
-    alignItems: "center",
-    alignSelf: "center",
-    justifyContent: "space-evenly",
-  },
-  button: {
-    flex: 1,
-    width: "100%",
-    borderRadius: 8,
-    backgroundColor: "#fff",
-    borderWidth: 1,
-    borderColor: "#ccc",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    gap: 60,
-  },
-  buttonText: {
-    color: "#1D3F72",
-    fontSize: 13,
-    fontWeight: "bold",
-  },
-});
 
 const generateBoxShadowStyle = (
   xOffset,
